473fe31a964fc2fcda972a7b4cda7195fcd95a85
danielballan <daniel.b.allan@gmail.com>
2017-05-30 15:13:01 -0400

    FIX: extract_title should be public

--- web_monitoring/utils.py
+++ web_monitoring/utils.py
@@ -2,7 +2,7 @@ import io
 import lxml.html
 
 
-def _extract_title(content_bytes):
+def extract_title(content_bytes):
     "Return content of <title> tag as string."
     content_as_file = io.StringIO(content_bytes.decode(errors='ignore'))
     title = lxml.html.parse(content_as_file).find(".//title").text
