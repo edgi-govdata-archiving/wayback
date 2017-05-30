1f4c0389d0cb7370b824348735234ecffea7a03c
danielballan <daniel.b.allan@gmail.com>
2017-05-30 16:12:20 -0400

    API: Make extract_title return empty string on failure
    Not all content has a title tag (or is even HTML).

--- web_monitoring/utils.py
+++ web_monitoring/utils.py
@@ -3,7 +3,13 @@ import lxml.html
 
 
 def extract_title(content_bytes):
-    "Return content of <title> tag as string."
+    "Return content of <title> tag as string. On failure return empty string."
     content_as_file = io.StringIO(content_bytes.decode(errors='ignore'))
-    title = lxml.html.parse(content_as_file).find(".//title").text
-    return title
+    try:
+        title = lxml.html.parse(content_as_file).find(".//title")
+    except Exception:
+        return ''
+    if title is None:
+        return ''
+    else:
+        return title.text
