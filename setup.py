41a7ad950dd27b385cd46b764f437dcf20728dc8
danielballan <daniel.b.allan@gmail.com>
2017-05-30 17:25:46 -0400

    Include install_requires by parsing requirements.txt
    Granted, the comments in requirements.txt were nice, but this use
    of requirements.txt is standard and the README should be sufficient.

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from distutils.core import setup
+import os
 import setuptools
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        result = f.read()
+    return result
 
 
 setup(name='web_monitoring',
       packages=['web_monitoring'],
       scripts=['scripts/wm'],
+      install_requires=read('requirements.txt').split(),
+      long_description=read('README.md'),
      )
