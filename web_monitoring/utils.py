907fe6e9b176eb0907b0cb2608080eea8e5b8d0f
danielballan <daniel.b.allan@gmail.com>
2017-05-31 08:33:47 -0400

    ENH: Accept optional encoding arg. Default behavior unchanged, just refactored.

--- web_monitoring/utils.py
+++ web_monitoring/utils.py
@@ -3,9 +3,11 @@ import io
 import lxml.html
 
 
-def extract_title(content_bytes):
+def extract_title(content_bytes, encoding='utf-8'):
     "Return content of <title> tag as string. On failure return empty string."
-    content_as_file = io.StringIO(content_bytes.decode(errors='ignore'))
+    content_str = content_bytes.decode(encoding=encoding, errors='ignore')
+    # The parser expects a file-like, so we mock one.
+    content_as_file = io.StringIO(content_str)
     try:
         title = lxml.html.parse(content_as_file).find(".//title")
     except Exception:
