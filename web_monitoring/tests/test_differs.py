6888c5d02bfcb3f2aba91faec550397dd5e0138a
Rob Brackett <rob@robbrackett.com>
2017-09-17 11:05:03 -0700

    Handle diffing HTML with differing node counts
    Fixes #91.

--- web_monitoring/tests/test_differs.py
+++ web_monitoring/tests/test_differs.py
@@ -50,12 +50,14 @@ def test_pagefreezer():
     # 2. Ensure that the resulting output is properly passed through
     pass
 
+
 def test_get_visible_text():
     html = '<!--First comment--><h1>First Heading</h1><p>First paragraph.</p>'
     actual = wd._get_visible_text(html)
     expected = ['First Heading',
                 'First paragraph.']
     assert actual == expected
+
 
 def test_html_diff_render():
     test_data = [('<html><head></head><body><p>Paragraph</p></body></html>',
@@ -67,15 +69,20 @@ def test_html_diff_render():
                  ('<html><head><title>HTML Diff Render</title></head><body><h1>Heading</h1></body></html>',
                   '<html><head><title>HTML Difference Render</title></head><body><h1>Head</h1></body></html>')]
 
-    test_results = ['<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style>\n <head></head>\n <body><h1><ins>Heading</ins></h1> <p><del>Paragraph</del></p></body>\n</html>',
-                    '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style>\n <body><h1><ins>Heading</ins></h1> <p>Paragraph</p></body>\n</html>',
-                    '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style>\n <body><h3>Paragraph</h3></body>\n</html>',
-                    '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style>\n <head><title>HTML <ins>Difference</ins> <del>Diff</del> Render</title></head>\n <body><h1><ins>Head</ins></h1> <h1><del>Heading</del></h1></body>\n</html>']
-
-    test_check = []
+    test_results = ['<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style> <head>\n </head> <body>\n  <h1><ins>Heading</ins></h1> <p><del>Paragraph</del></p>\n </body>\n</html>',
+                    '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style> <body>\n  <h1><ins>Heading</ins></h1> <p>Paragraph</p>\n </body>\n</html>',
+                    '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style> <body>\n  <h3>Paragraph</h3>\n </body>\n</html>',
+                    '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style> <head>\n  <title>HTML <ins>Difference</ins> <del>Diff</del> Render</title>\n </head> <body>\n  <h1><ins>Head</ins></h1> <h1><del>Heading</del></h1>\n </body>\n</html>']
 
     for index in range(len(test_data)):
-        test_check.append(wd.html_diff_render(test_data[index][0],
-                                              test_data[index][1]) == test_results[index])
+        diff = wd.html_diff_render(test_data[index][0], test_data[index][1])
+        assert (f'{index}: ' + test_results[index]) == (f'{index}: ' + diff)
 
-    assert False not in test_check
+
+def test_html_diff_render_handles_differing_numbers_of_nodes():
+    test_data = ('<html><head></head><body><p>Paragraph</p></body></html>',
+                 '<html><head></head>\n<body><h1>Heading</h1></body></html>')
+
+    expected = '<html>\n <style type="text/css">\n  ins {text-decoration : none; background-color: #d4fcbc;}\n                        del {text-decoration : none; background-color: #fbb6c2;}\n </style> <head>\n </head> <body>\n  <h1><ins>Heading</ins></h1> <p><del>Paragraph</del></p>\n </body>\n</html>'
+    diff = wd.html_diff_render(test_data[0], test_data[1])
+    assert expected == diff
