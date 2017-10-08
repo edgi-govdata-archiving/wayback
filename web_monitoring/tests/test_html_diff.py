4fdb0076ee81591de9e1b9e58217623006f8d708
danielballan <daniel.b.allan@gmail.com>
2017-10-08 15:42:21 -0400

    Add test on real example HTML.

--- web_monitoring/tests/test_html_diff.py
+++ web_monitoring/tests/test_html_diff.py
@@ -1,6 +1,7 @@
 from pkg_resources import resource_filename
 import pytest
 import htmltreediff
+from web_monitoring.db import fetch_version_content
 from web_monitoring.differs import html_diff_render
 from htmldiffer.diff import HTMLDiffer
 
@@ -40,7 +41,7 @@ cases = ['change-tag',
 # flag to see these outputs.
 
 @pytest.mark.parametrize('fn', cases)
-def test_htmltreediff(fn):
+def test_contrived_examples_htmltreediff(fn):
     before, after = lookup_pair(fn)
     d = htmltreediff.diff(before, after, ins_tag='diffins', del_tag='diffdel',
                           pretty=True)
@@ -48,14 +49,56 @@ def test_htmltreediff(fn):
 
 
 @pytest.mark.parametrize('fn', cases)
-def test_html_diff_render(fn):
+def test_contrived_examples_html_diff_render(fn):
     before, after = lookup_pair(fn)
     d = html_diff_render(before, after)
     print(TEMPLATE.format(before, after, d))
 
 
 @pytest.mark.parametrize('fn', cases)
-def test_htmldiffer(fn):
+def test_contrived_examples_htmldiffer(fn):
     before, after = lookup_pair(fn)
     d = HTMLDiffer(before, after).combined_diff
     print(TEMPLATE.format(before, after, d))
+
+
+### TESTS ON MORE COMPLEX, REAL CASES
+
+# TODO These UUIDs refer to the staging app and therefore assume that the
+# env variable WEB_MONITORING_DB_URL is pointed at staging.
+version_ids = [
+     ('f2d5d701-707a-42e0-8881-653346d01e0a',
+      'fc74d750-c651-46b7-bf74-434ad8c62e04'),
+    ]
+
+# Fetch content as we need it, and cache. This can potentially matter if a
+# subset of the tests are run.
+version_content_cache = {}
+
+def get_content(version_id):
+    try:
+        return version_content_cache[version_id]
+    except KeyError:
+        content = fetch_version_content(version_id).decode()
+        version_content_cache[version_id] = content
+        return content
+
+
+@pytest.mark.parametrize('before_id, after_id', version_ids)
+def test_real_examples_htmltreediff(before_id, after_id):
+    before, after = get_content(before_id), get_content(after_id)
+    htmltreediff.diff(before, after,
+                      ins_tag='diffins',del_tag='diffdel',
+                      pretty=True)
+
+
+@pytest.mark.parametrize('before_id, after_id', version_ids)
+def test_real_examples_html_diff_render(before_id, after_id):
+    before, after = get_content(before_id), get_content(after_id)
+    html_diff_render(before, after)
+
+
+@pytest.mark.parametrize('before_id, after_id', version_ids)
+def test_real_examples_htmldiffer(before_id, after_id):
+    before, after = get_content(before_id), get_content(after_id)
+    HTMLDiffer(before, after).combined_diff
