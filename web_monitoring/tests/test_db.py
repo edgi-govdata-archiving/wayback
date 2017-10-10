7abb6f4246119db576c2c837f0ef77f1199906e8
danielballan <daniel.b.allan@gmail.com>
2017-10-10 14:56:02 -0400

    Refactor into class to allow multiple connects (plus a few unrelated fixes).

--- web_monitoring/tests/test_db.py
+++ web_monitoring/tests/test_db.py
@@ -6,7 +6,7 @@
 from datetime import datetime, timedelta, timezone
 import os
 import pytest
-import web_monitoring.db as wdb
+from web_monitoring.db import Client, MissingCredentials
 import vcr
 
 
@@ -30,117 +30,122 @@ TO_VERSION_ID = '9342c121-cff0-4454-934f-d0f118508da1'
 VERSIONISTA_ID = '10329339'
 
 # The only matters when re-recording the tests for vcr.
-SETTINGS = {}
-SETTINGS['db_url'] = "http://localhost:3000"
-SETTINGS['db_email'] = "seed-admin@example.com"
-SETTINGS['db_password'] = "PASSWORD"
+AUTH = {'url': "http://localhost:3000",
+        'email': "seed-admin@example.com",
+        'password': "PASSWORD"}
 
 
 def test_missing_creds():
-    # Clear out settings to override settings from environment variables.
-    for k in wdb.settings:
-        wdb.settings[k] = None
-    # Test that we fail early if no credenitals are entered.
-    with pytest.raises(wdb.MissingCredentials):
-        wdb.list_pages()
+    try:
+        env = os.environ.copy()
+        os.environ.clear()
+        with pytest.raises(MissingCredentials):
+            Client.from_env()
+        os.environ.update({'WEB_MONITORING_DB_URL': AUTH['url'],
+                           'WEB_MONITORING_DB_EMAIL': AUTH['email'],
+                           'WEB_MONITORING_DB_PASSWORD': AUTH['password']})
+        Client.from_env()  # should work
+    finally:
+        os.environ.update(env)
 
 
 @db_vcr.use_cassette()
 def test_list_pages():
-    wdb.settings = SETTINGS
-    res = wdb.list_pages()
+    cli = Client(**AUTH)
+    res = cli.list_pages()
     assert res['data']
 
     # Test chunk query parameters.
-    res = wdb.list_pages(chunk_size=2)
+    res = cli.list_pages(chunk_size=2)
     assert len(res['data']) == 2
-    res = wdb.list_pages(chunk_size=5)
+    res = cli.list_pages(chunk_size=5)
     assert len(res['data']) == 5
 
     # Test filtering query parameters.
-    res = wdb.list_pages(url='__nonexistent__')
+    res = cli.list_pages(url='__nonexistent__')
     assert len(res['data']) == 0
-    res = wdb.list_pages(url=URL)
+    res = cli.list_pages(url=URL)
     assert len(res['data']) > 0
-    res = wdb.list_pages(site='__nonexistent__')
+    res = cli.list_pages(site='__nonexistent__')
     assert len(res['data']) == 0
-    res = wdb.list_pages(site=SITE)
+    res = cli.list_pages(site=SITE)
     assert len(res['data']) > 0
-    res = wdb.list_pages(agency='__nonexistent__')
+    res = cli.list_pages(agency='__nonexistent__')
     assert len(res['data']) == 0
-    res = wdb.list_pages(agency=AGENCY)
+    res = cli.list_pages(agency=AGENCY)
     assert len(res['data']) > 0
 
 
 @db_vcr.use_cassette()
 def test_get_page():
-    wdb.settings = SETTINGS
-    res = wdb.get_page(PAGE_ID)
+    cli = Client(**AUTH)
+    res = cli.get_page(PAGE_ID)
     assert res['data']['uuid'] == PAGE_ID
 
 
 @db_vcr.use_cassette()
 def test_list_page_versions():
-    wdb.settings = SETTINGS
-    res = wdb.list_versions(page_id=PAGE_ID)
+    cli = Client(**AUTH)
+    res = cli.list_versions(page_id=PAGE_ID)
     assert all([v['page_uuid'] == PAGE_ID for v in res['data']])
 
 
 @db_vcr.use_cassette()
 def test_list_versions():
-    wdb.settings = SETTINGS
-    res = wdb.list_versions()
+    cli = Client(**AUTH)
+    res = cli.list_versions()
     assert res['data']
 
 
 @db_vcr.use_cassette()
 def test_get_version():
-    wdb.settings = SETTINGS
-    res = wdb.get_version(TO_VERSION_ID)
+    cli = Client(**AUTH)
+    res = cli.get_version(TO_VERSION_ID)
     assert res['data']['uuid'] == TO_VERSION_ID
     assert res['data']['page_uuid'] == PAGE_ID
 
 
 @db_vcr.use_cassette()
 def test_get_version_by_versionista_id():
-    wdb.settings = SETTINGS
-    res = wdb.get_version_by_versionista_id(VERSIONISTA_ID)
+    cli = Client(**AUTH)
+    res = cli.get_version_by_versionista_id(VERSIONISTA_ID)
     assert res['data']['uuid'] == TO_VERSION_ID
     assert res['data']['page_uuid'] == PAGE_ID
 
 
 @db_vcr.use_cassette()
 def test_get_version_by_versionista_id_failure():
+    cli = Client(**AUTH)
     with pytest.raises(ValueError):
-        wdb.get_version_by_versionista_id('__nonexistent__')
+        cli.get_version_by_versionista_id('__nonexistent__')
 
 
 @db_vcr.use_cassette()
 def test_get_version_uri_from_versionista():
-    wdb.settings = SETTINGS
+    cli = Client(**AUTH)
     # smoke test (because URI is installation-dependent)
-    wdb.get_version_uri(VERSIONISTA_ID, id_type='source')
+    cli.get_version_uri(VERSIONISTA_ID, id_type='source')
 
 
 @db_vcr.use_cassette()
 def test_get_version_uri_from_db():
-    wdb.settings = SETTINGS
+    cli = Client(**AUTH)
     # smoke test (because URI is installation-dependent)
-    wdb.get_version_uri(TO_VERSION_ID, id_type='db')
+    cli.get_version_uri(TO_VERSION_ID, id_type='db')
 
 
 @db_vcr.use_cassette()
 def test_add_version():
-    wdb.settings = SETTINGS
+    cli = Client(**AUTH)
     new_version_id = '06620776-d347-4abd-a423-a871620299b4'
     now = datetime.now(timezone.utc)
-    wdb.add_version(page_id=PAGE_ID, uuid=new_version_id,
+    cli.add_version(page_id=PAGE_ID, uuid=new_version_id,
                      capture_time=now,
                      uri='http://example.com',
                      hash='hash_placeholder',
                      title='title_placeholder',
                      source_type='test')
-    data = wdb.get_version(new_version_id)['data']
+    data = cli.get_version(new_version_id)['data']
     assert data['uuid'] == new_version_id
     assert data['page_uuid'] == PAGE_ID
     # Some floating-point error occurs in round-trip.
@@ -150,33 +155,35 @@ def test_add_version():
 
 
 def test_add_versions():
+    cli = Client(**AUTH)
     pass
 
 
-def test_query_import_status():
+def test_get_import_status():
+    cli = Client(**AUTH)
     pass
 
 
 @db_vcr.use_cassette()
 def test_list_changes():
+    cli = Client(**AUTH)
     # smoke test
-    wdb.settings = SETTINGS
-    wdb.list_changes(PAGE_ID)
+    cli.list_changes(PAGE_ID)
 
 
 @db_vcr.use_cassette()
 def test_get_change():
+    cli = Client(**AUTH)
     # smoke test
-    wdb.settings = SETTINGS
-    wdb.get_change(page_id=PAGE_ID,
+    cli.get_change(page_id=PAGE_ID,
                    to_version_id=TO_VERSION_ID)
 
 
 @db_vcr.use_cassette()
 def test_list_annotations():
+    cli = Client(**AUTH)
     # smoke test
-    wdb.settings = SETTINGS
-    wdb.list_annotations(page_id=PAGE_ID,
+    cli.list_annotations(page_id=PAGE_ID,
                          to_version_id=TO_VERSION_ID)
 
 
@@ -186,10 +193,10 @@ mutable_stash = []  # used to pass info from this test to the next
 
 @db_vcr.use_cassette()
 def test_add_annotation():
+    cli = Client(**AUTH)
     # smoke test
-    wdb.settings = SETTINGS
     annotation = {'foo': 'bar'}
-    result = wdb.add_annotation(annotation,
+    result = cli.add_annotation(annotation,
                                  page_id=PAGE_ID,
                                  to_version_id=TO_VERSION_ID)
     annotation_id = result['data']['uuid']
@@ -198,9 +205,9 @@ def test_add_annotation():
 
 @db_vcr.use_cassette()
 def test_get_annotation():
-    wdb.settings = SETTINGS
+    cli = Client(**AUTH)
     annotation_id = mutable_stash.pop()
-    result = wdb.get_annotation(annotation_id,
+    result = cli.get_annotation(annotation_id,
                                 page_id=PAGE_ID,
                                 to_version_id=TO_VERSION_ID)
     fetched_annotation = result['data']['annotation']
