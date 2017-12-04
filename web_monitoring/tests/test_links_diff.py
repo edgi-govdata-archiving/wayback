0070b74355e232b1ce3ecc99238201c2c27a1f60
Rob Brackett <rob@robbrackett.com>
2017-12-04 00:19:50 -0800

    Add basic version of links diff, fixes #102
    This can be reached at the `/links_diff` endpoint.

---
+++ web_monitoring/tests/test_links_diff.py
@@ -0,0 +1,30 @@
+import pytest
+import re
+from web_monitoring.links_diff import links_diff
+
+
+def test_links_diff_only_includes_links():
+    html_a = """
+             Here is some HTML with <a href="http://google.com">some links</a>
+             in it. Those links <a href="http://example.com">go places</a>.
+             """
+    html_b = """
+             Here is some HTML with <a href="http://ugh.com">some</a> links
+             in it. Those links <a href="http://example.com">go places</a>.
+             """
+    result = links_diff(html_a, html_b)
+    assert 'Here is some' not in result
+    assert '<li>go places' in result
+
+
+def test_links_diff_only_has_outgoing_links():
+    html_a = """
+             Here is some HTML with <a href="http://google.com">some links</a>
+             in it. Those links <a href="#local">go places</a>.
+             """
+    html_b = """
+             Here is some HTML with <a href="http://google.com">some links</a>
+             in it. Those links <a href="#local">go places</a>.
+             """
+    result = links_diff(html_a, html_b)
+    assert result.count('<a') == 1
