e07622ba0808d35d6f9c94e839dd7778ce34f8e0
Rob Brackett <rob@robbrackett.com>
2017-12-20 18:55:42 -0800

    Address review comments

--- web_monitoring/__init__.py
+++ web_monitoring/__init__.py
@@ -1,3 +1,9 @@
+import logging
+import os
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+if os.environ.get('LOG_LEVEL'):
+    logging.basicConfig(level=os.environ['LOG_LEVEL'].upper())
