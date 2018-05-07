5e61368e2759cb55cbbfd9c5c62e5076806ab2df
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-05-07 20:22:10 +0300

    Allocating a new port for each test, all tests working

--- web_monitoring/tests/test_diffing_server_exc_handling.py
+++ web_monitoring/tests/test_diffing_server_exc_handling.py
@@ -6,35 +6,35 @@ class DiffingServerExceptionHandlingTest(AsyncHTTPTestCase):
 
     def get_app(self):
         app = df.make_app()
-        app.listen(8888)
+        app.listen(str(self.get_http_port()))
         return
 
     def testInvalidURLaFormat(self):
-        response = self.fetch('http://localhost:8888/html_token?format=json&include=all&a=example.org&b=https://example.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&a=example.org&b=https://example.org', method='GET')
         self.assertEqual(response.code, 400)
 
     def testInvalidURLbFormat(self):
-        response = self.fetch('http://localhost:8888/html_token?format=json&include=all&a=https://example.org&b=example.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&a=https://example.org&b=example.org', method='GET')
         self.assertEqual(response.code, 400)
 
     def testInvalidDiffingMethod(self):
-        response = self.fetch('http://localhost:8888/non_existing?format=json&include=all&a=example.org&b=https://example.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/non_existing?format=json&include=all&a=example.org&b=https://example.org', method='GET')
         self.assertEqual(response.code, 404)
     
     def testMissingURLa(self):
-        response = self.fetch('http://localhost:8888/html_token?format=json&include=all&b=https://example.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&b=https://example.org', method='GET')
         self.assertEqual(response.code, 400)
 
     def testMissingURLb(self):
-        response = self.fetch('http://localhost:8888/html_token?format=json&include=all&a=https://example.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&a=https://example.org', method='GET')
         self.assertEqual(response.code, 400)
 
     def testNotReachableURLa(self):
-        response = self.fetch('http://localhost:8888/html_token?format=json&include=all&a=https://eeexample.org&b=https://example.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&a=https://eeexample.org&b=https://example.org', method='GET')
         self.assertEqual(response.code, 400)
 
     def testNotReachableURLb(self):
-        response = self.fetch('http://localhost:8888/html_token?format=json&include=all&a=https://example.org&b=https://eeexample.org', method='GET')
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&a=https://example.org&b=https://eeexample.org', method='GET')
         self.assertEqual(response.code, 400)
 
     def testMissingParamsCallerFunc(self):
@@ -47,12 +47,9 @@ class DiffingServerExceptionHandlingTest(AsyncHTTPTestCase):
         with self.assertRaises(UndecodableContentError):
             df._decode_body(response,'a',False)
 
-# This test breaks them all
-    # def testFetchUndecodableContent(self):
-    #     print('here')
-    #     response = self.fetch('http://localhost:8888/html_token?format=json&include=all&a=https://example.org&b=https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt')
-    #     print("also here")
-    #     self.assertEqual(response.code, 422)
+    def testFetchUndecodableContent(self):
+        response = self.fetch('http://localhost:'+str(self.get_http_port())+'/html_token?format=json&include=all&a=https://example.org&b=https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt')
+        self.assertEqual(response.code, 422)
 
 def mockDiffingMethod(c_body):
         return
