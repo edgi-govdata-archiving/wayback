dd8df0407610a56c9cced421b797b328bc01628f
Rob Brackett <rob@robbrackett.com>
2018-05-16 10:26:40 -0700

    Do some light style cleanup on exception tests
    - Remove unnecessary `f` strings
    - Make sure helper method is after all the test methods

--- web_monitoring/tests/test_diffing_server_exc_handling.py
+++ web_monitoring/tests/test_diffing_server_exc_handling.py
@@ -10,44 +10,44 @@ class DiffingServerExceptionHandlingTest(AsyncHTTPTestCase):
         return df.make_app()
 
     def test_invalid_url_a_format(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'a=example.org&b=https://example.org')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'a=example.org&b=https://example.org')
         self.json_check(response)
         self.assertEqual(response.code, 400)
 
     def test_invalid_url_b_format(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'a=https://example.org&b=example.org')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'a=https://example.org&b=example.org')
         self.json_check(response)
         self.assertEqual(response.code, 400)
 
     def test_invalid_diffing_method(self):
-        response = self.fetch(f'/non_existing?format=json&include=all&'
-                              f'a=example.org&b=https://example.org')
+        response = self.fetch('/non_existing?format=json&include=all&'
+                              'a=example.org&b=https://example.org')
         self.json_check(response)
         self.assertEqual(response.code, 404)
 
     def test_missing_url_a(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'b=https://example.org')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'b=https://example.org')
         self.json_check(response)
         self.assertEqual(response.code, 400)
 
     def test_missing_url_b(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'a=https://example.org')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'a=https://example.org')
         self.json_check(response)
         self.assertEqual(response.code, 400)
 
     def test_not_reachable_url_a(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'a=https://eeexample.org&b=https://example.org')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'a=https://eeexample.org&b=https://example.org')
         self.json_check(response)
         self.assertEqual(response.code, 400)
 
     def test_not_reachable_url_b(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'a=https://example.org&b=https://eeexample.org')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'a=https://example.org&b=https://eeexample.org')
         self.json_check(response)
         self.assertEqual(response.code, 400)
 
@@ -63,12 +63,19 @@ class DiffingServerExceptionHandlingTest(AsyncHTTPTestCase):
             df._decode_body(response, 'a', False)
 
     def test_fetch_undecodable_content(self):
-        response = self.fetch(f'/html_token?format=json&include=all&'
-                              f'a=https://example.org&'
-                              f'b=https://www.cl.cam.ac.uk'
-                              f'/~mgk25/ucs/examples/UTF-8-test.txt')
+        response = self.fetch('/html_token?format=json&include=all&'
+                              'a=https://example.org&'
+                              'b=https://www.cl.cam.ac.uk'
+                              '/~mgk25/ucs/examples/UTF-8-test.txt')
         self.json_check(response)
         self.assertEqual(response.code, 422)
+
+    def test_a_is_404(self):
+        response = self.fetch('/html_token?format=json&include=all'
+                              '&a=http://httpstat.us/404'
+                              '&b=https://example.org')
+        self.assertEqual(response.code, 404)
+        self.json_check(response)
 
     def json_check(self, response):
         json_header = response.headers.get('Content-Type').split(';')
@@ -77,13 +84,6 @@ class DiffingServerExceptionHandlingTest(AsyncHTTPTestCase):
         json_response = json.loads(response.body)
         self.assertTrue(isinstance(json_response['code'], int))
         self.assertTrue(isinstance(json_response['error'], str))
-
-    def test_a_is_404(self):
-        response = self.fetch(f'/html_token?format=json&include=all'
-                              f'&a=http://httpstat.us/404'
-                              f'&b=https://example.org')
-        self.assertEqual(response.code, 404)
-        self.json_check(response)
 
 
 def mock_diffing_method(c_body):
