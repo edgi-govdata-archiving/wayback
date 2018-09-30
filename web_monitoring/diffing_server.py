f72831e084bd901c728bbb16198c0986b984f9a8
Vangelis Banos <vangelis@archive.org>
2018-09-30 15:07:27 +0000

    Support multiple headers in pass_header param
    Parameter `pass_header` can have multiple comma separated values like
    this: `pass_header=cookie,accept,other`

--- web_monitoring/diffing_server.py
+++ web_monitoring/diffing_server.py
@@ -136,15 +136,17 @@ class DiffHandler(BaseHandler):
                     body = f.read()
                     responses[param] = MockResponse(url, body, headers)
         # Now fetch any nonlocal URLs.
-        # Pass request header defined by URL param pass_header=HEADER_NAME
+        # Pass request headers defined by URL param pass_header=HEADER_NAME
         # to nonlocal URLs. Useful for passing data like cookie headers.
+        # HEADER_NAME can be one or multiple headers separated by ','
         to_fetch = {k: v for k, v in urls.items() if k not in responses}
         headers = {}
-        header_key = query_params.get('pass_header')
-        if header_key:
-            header_value = self.request.headers.get(header_key)
-            if header_value:
-                headers[header_key] = header_value
+        header_keys = query_params.get('pass_header')
+        if header_keys:
+            for header_key in header_keys.split(','):
+                header_value = self.request.headers.get(header_key)
+                if header_value:
+                    headers[header_key] = header_value
 
         fetched = yield [client.fetch(url, headers=headers, raise_error=False)
                          for url in to_fetch.values()]
