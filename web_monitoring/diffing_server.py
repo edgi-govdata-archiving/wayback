158d0a6a65169c932b06c8496983db77d346fa57
Rob Brackett <rob@robbrackett.com>
2018-11-08 00:11:09 -0800

    Handle decoding empty content, fixes #311

--- web_monitoring/diffing_server.py
+++ web_monitoring/diffing_server.py
@@ -325,13 +325,17 @@ def _decode_body(response, name, raise_if_binary=True):
         # If the encoding we found isn't known, fall back to ascii
         text = response.body.decode('ascii', errors='replace')
 
+    text_length = len(text)
+    if text_length == 0:
+        return text
+
     # Replace null terminators; some differs (especially those written in C)
     # don't handle them well in the middle of a string.
     text = text.replace('\u0000', '\ufffd')
 
     # If a significantly large portion of the document was totally undecodable,
     # it's likely this wasn't text at all, but binary data.
-    if raise_if_binary and text.count('\ufffd') / len(text) > 0.25:
+    if raise_if_binary and text.count('\ufffd') / text_length > 0.25:
         raise UndecodableContentError(f'The response body of `{name}` could not be decoded as {encoding}.')
 
     return text
