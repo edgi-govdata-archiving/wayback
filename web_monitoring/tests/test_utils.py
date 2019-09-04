b362860f8fb83197131811213dfbe69253a9167a
Rob Brackett <rob@robbrackett.com>
2019-09-04 15:48:26 -0700

    Add command to import known page URLs from IA (#174)
    The new `wm import ia-known-pages` command imports from the Internet Archive Wayback Machine only the pages we already have listed in a web-monitoring-db database.
    
    This adds a slew of new functionality and complex logic to our `WaybackClient` — we now handle most of the types of errors we’ve seen from Wayback APIs in a much more robust way.
    
    The overall import process has also gained a fair bit of complexity. Some of it we can hopefully clean up and smooth over some more, but in order to perform at the full scale of the number of pages we care about, it necessarily needs a lot of async/thread-based logic. Operationally, this looks something like:
    
    ```
     ┌──────────────┐
     │ Load list of │
     │  known URLs  │
     └──────────────┘
            │
     ┌─ ─ ─ ┼ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ (in parallel) ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐
     ┊      │                                                                     ┊
     ┊ ┌──────────┐  ┌─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐  ┌────────┐ ┊
     ┊ │ Load CDX │  ┊ ┌────────────┐ ┌────────────┐ ┌────────────┐ ┊  │ Import │ ┊
     ┊ │ records  │  ┊ │Load memento│ │Load memento│ │Load memento│ ┊  │   to   │ ┊
     ┊ │ for URLs │  ┊ └────────────┘ └────────────┘ └────────────┘ ┊  │   DB   │ ┊
     ┊ │          │  ┊    ├─────────────────┴──────────────┘        ┊  │        │ ┊
     ┊ └──────────┘  └─ ─ ┼ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┘  └────────┘ ┊
     ┊      ↓             ↑                              ↓   ↓           ↑   │    ┊
     ┊      └── (queue) ──┘ <─── (re-queue errors x2) ───┘   └─ (queue) ─┘   │    ┊
     ┊                                                                       │    ┊
     └─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─┼─ ─ ┘
                                                                             │
                                                                           Done!
    ```
    
    There are also a few support classes for handling signals, async iteration, queue management, etc.
    
    Fixes #86.
    
    This also includes a couple fixes to how we handle database API issues that have changed while this PR was in progress:
    
    - Specify `status` as top-level import property
    
        Way back at the end of last year, we added `status` as a first-class field on versions (edgi-govdata-archiving/web-monitoring-db#453), but never switched to actually sending it properly in our import data! This moves it from `source_metadata` (where it would be redundant) to the new first-class field.
    
    - Use `include_total=True` when sampling URLs
    
        The web-monitoring-db project is removing totals from list queries in order to improve performance (counting the total possible results is super expensive). You can still get the totals by using the `?include_total` query parameter, so this just makes sure to set that in the one query where we need that information (determining the size of the population to run a random sample over).
    
        See the relevant change in the API: https://github.com/edgi-govdata-archiving/web-monitoring-db/pull/596

--- web_monitoring/tests/test_utils.py
+++ web_monitoring/tests/test_utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-import requests_mock
-from web_monitoring.utils import extract_title, retryable_request, rate_limited
+import queue
+import threading
+from web_monitoring.utils import extract_title, rate_limited, FiniteQueue
 
 
 def test_extract_title():
@@ -17,6 +18,21 @@ def test_extract_title_from_titleless_page():
         <body>Blah</body>
     </html>''')
     assert title == ''
+
+
+def test_extract_title_handles_whitespace():
+    title = extract_title(b'''<html>
+        <head>
+            <meta charset="utf-8">
+            <title>
+
+                THIS IS
+                THE  TITLE
+            </title>
+        </head>
+        <body>Blah</body>
+    </html>''')
+    assert title == 'THIS IS THE TITLE'
 
 
 def test_rate_limited():
@@ -45,38 +61,43 @@ def test_separate_rate_limited_groups_do_not_affect_each_other():
     assert duration.total_seconds() < 0.55
 
 
-def test_retryable_request_retries():
-    with requests_mock.Mocker() as mock:
-        mock.get('http://test.com', [{'text': 'bad', 'status_code': 503},
-                                     {'text': 'good', 'status_code': 200}])
-        response = retryable_request('GET', 'http://test.com', backoff=0)
-        assert response.ok
+class TestFiniteQueue:
+    def test_queue_ends_with_QUEUE_END(self):
+        test_queue = FiniteQueue()
+        test_queue.put('First')
+        test_queue.end()
 
+        assert test_queue.get() == 'First'
+        assert test_queue.get() is FiniteQueue.QUEUE_END
+        # We should keep getting QUEUE_END from now on, too.
+        assert test_queue.get() is FiniteQueue.QUEUE_END
 
-def test_retryable_request_stops_after_given_retries():
-    with requests_mock.Mocker() as mock:
-        mock.get('http://test.com', [{'text': 'bad1', 'status_code': 503},
-                                     {'text': 'bad2', 'status_code': 503},
-                                     {'text': 'bad3', 'status_code': 503},
-                                     {'text': 'good', 'status_code': 200}])
-        response = retryable_request('GET', 'http://test.com', retries=2, backoff=0)
-        assert response.status_code == 503
-        assert response.text == 'bad3'
+    def test_queue_is_iterable(self):
+        test_queue = FiniteQueue()
+        test_queue.put('First')
+        test_queue.end()
 
+        result = list(test_queue)
+        assert result == ['First']
 
-def test_retryable_request_only_retries_gateway_errors():
-    with requests_mock.Mocker() as mock:
-        mock.get('http://test.com', [{'text': 'bad1', 'status_code': 400},
-                                     {'text': 'good', 'status_code': 200}])
-        response = retryable_request('GET', 'http://test.com', backoff=0)
-        assert response.status_code == 400
+    def test_queue_can_be_safely_read_from_multiple_threads(self):
+        # We want to make sure that a thread can't get stuck waiting for the
+        # next item on a queue that is already ended.
+        test_queue = FiniteQueue()
+        results = queue.SimpleQueue()
 
+        def read_one_item():
+            try:
+                results.put(test_queue.get(timeout=1))
+            except queue.Empty as error:
+                results.put(error)
 
-def test_retryable_request_with_custom_retry_logic():
-    with requests_mock.Mocker() as mock:
-        mock.get('http://test.com', [{'text': 'bad1', 'status_code': 400},
-                                     {'text': 'good', 'status_code': 200}])
+        threads = [threading.Thread(target=read_one_item) for i in range(3)]
+        [thread.start() for thread in threads]
+        test_queue.put('First')
+        test_queue.end()
+        [thread.join() for thread in threads]
 
-        response = retryable_request('GET', 'http://test.com', backoff=0,
-                                     should_retry=lambda r: r.status_code == 400)
-        assert response.status_code == 200
+        assert results.get() == 'First'
+        assert results.get() is FiniteQueue.QUEUE_END
+        assert results.get() is FiniteQueue.QUEUE_END
