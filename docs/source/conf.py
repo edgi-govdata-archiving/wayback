759bbb4fa1218b58391d1ca268216146fe8665c5
Dan Allan <daniel.b.allan@gmail.com>
2019-11-17 13:19:37 -0500

    Remove matplotlib sphinx extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,15 +40,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting',
-    'matplotlib.sphinxext.plot_directive',
     'numpydoc',
     'sphinx_copybutton',
 ]
-
-# Configuration options for plot_directive. See:
-# https://github.com/matplotlib/matplotlib/blob/f3ed922d935751e08494e5fb5311d3050a3b637b/lib/matplotlib/sphinxext/plot_directive.py#L81
-plot_html_show_source_link = False
-plot_html_show_formats = False
 
 # Generate the API documentation when building
 autosummary_generate = True
