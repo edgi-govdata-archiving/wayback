7f0fdccc8b62dec54936619c677660807f514b5d
Rob Brackett <rob@robbrackett.com>
2019-12-02 14:12:34 -0800

    Remove vestigial bits from `_utils` module (#10)
    * Remove vestigial `_utils.extract_title`
    
    * Remove vestigial `utils.hash_content`
    
    * Remove vestigial `util.get_color_palette`
    
    * Remove vestigial `FiniteQueue`, `iterate_into_queue`
    
    * Remove vestigial `Signal` and `QuitSignal`
    
    * Remove vestigial `lxml` dependency
    
    This was needed for `utils.extract_title`, which was also vestigial and removed in a previous commit.
    
    * Move `SessionClosedError` to `exceptions` module
    
    * Remove `_utils.logger`
    
    We never actually use this logger, so it doesn't make much sense to create.

--- wayback/_utils.py
+++ wayback/_utils.py
@@ -1,45 +1,10 @@
 from collections import defaultdict
 from contextlib import contextmanager
-import hashlib
-import io
-import logging
-import os
-import queue
-import re
-import signal
-import threading
-import time
-
-import lxml.html
 import requests
 import requests.adapters
-
-
-logger = logging.getLogger(__name__)
-
-WHITESPACE_PATTERN = re.compile(r'\s+')
-
-
-def extract_title(content_bytes, encoding='utf-8'):
-    "Return content of <title> tag as string. On failure return empty string."
-    content_str = content_bytes.decode(encoding=encoding, errors='ignore')
-    # The parser expects a file-like, so we mock one.
-    content_as_file = io.StringIO(content_str)
-    try:
-        title = lxml.html.parse(content_as_file).find(".//title")
-    except Exception:
-        return ''
-
-    if title is None or title.text is None:
-        return ''
-
-    # In HTML, all consecutive whitespace (including line breaks) collapses
-    return WHITESPACE_PATTERN.sub(' ', title.text.strip())
-
-
-def hash_content(content_bytes):
-    "Create a version_hash for the content of a snapshot."
-    return hashlib.sha256(content_bytes).hexdigest()
+import threading
+import time
+from .exceptions import SessionClosedError
 
 
 _last_call_by_group = defaultdict(int)
@@ -72,83 +37,6 @@ def rate_limited(calls_per_second=2, group='default'):
                 time.sleep(minimum_wait - (current_time - last_call))
             _last_call_by_group[group] = time.time()
         yield
-
-
-def get_color_palette():
-    """
-    Read and return the CSS color env variables that indicate the colors in
-    html_diff_render, differs and links_diff.
-
-    Returns
-    ------
-    palette: Dictionary
-        A dictionary containing the differ_insertion and differ_deletion css
-        color codes
-    """
-    differ_insertion = os.environ.get('DIFFER_COLOR_INSERTION', '#a1d76a')
-    differ_deletion = os.environ.get('DIFFER_COLOR_DELETION', '#e8a4c8')
-    return {'differ_insertion': differ_insertion,
-            'differ_deletion': differ_deletion}
-
-
-def iterate_into_queue(queue, iterable):
-    """
-    Read items from an iterable and place them onto a FiniteQueue.
-
-    Parameters
-    ----------
-    queue: FiniteQueue
-    iterable: sequence
-    """
-    for item in iterable:
-        queue.put(item)
-    queue.end()
-
-
-class FiniteQueue(queue.SimpleQueue):
-    """
-    A queue that is iterable, with a defined end.
-
-    The end of the queue is indicated by the `FiniteQueue.QUEUE_END` object.
-    If you are using the iterator interface, you won't ever encounter it, but
-    if reading the queue with `queue.get`, you will receive
-    `FiniteQueue.QUEUE_END` if you’ve reached the end.
-    """
-
-    # Use a class instad of `object()` for more readable names for debugging.
-    class QUEUE_END:
-        ...
-
-    def __init__(self):
-        super().__init__()
-        self._ended = False
-        # The Queue documentation suggests that put/get calls can be
-        # re-entrant, so we need to use RLock here.
-        self._lock = threading.RLock()
-
-    def end(self):
-        self.put(self.QUEUE_END)
-
-    def get(self, *args, **kwargs):
-        with self._lock:
-            if self._ended:
-                return self.QUEUE_END
-            else:
-                value = super().get(*args, **kwargs)
-                if value is self.QUEUE_END:
-                    self._ended = True
-
-                return value
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        item = self.get()
-        if item is self.QUEUE_END:
-            raise StopIteration
-
-        return item
 
 
 class DepthCountedContext:
@@ -185,10 +73,6 @@ class DepthCountedContext:
         pass
 
 
-class SessionClosedError(Exception):
-    ...
-
-
 class DisableAfterCloseSession(requests.Session):
     """
     A custom session object raises a :class:`SessionClosedError` if you try to
@@ -209,96 +93,3 @@ class DisableAfterCloseSession(requests.Session):
                                      'and cannot send new HTTP requests.')
 
         return super().send(*args, **kwargs)
-
-
-class Signal:
-    """
-    A context manager to handle signals from the system safely. It keeps track
-    of previous signal handlers and ensures that they are put back into place
-    when the context exits.
-
-    Parameters
-    ----------
-    signals : int or tuple of int
-        The signal or list of signals to handle.
-    handler : callable
-        A signal handler function of the same type used with `signal.signal()`.
-        See: https://docs.python.org/3.6/library/signal.html#signal.signal
-
-    Examples
-    --------
-    Ignore SIGINT (ctrl+c) and print a glib message instead of quitting:
-
-    >>> def ignore_signal(signal_type, frame):
-    >>>     print("Sorry, but you can't quit this program that way!")
-    >>>
-    >>> with Signal((signal.SIGINT, signal.SIGTERM), ignore_signal):
-    >>>     do_some_work_that_cant_be_interrupted()
-    """
-    def __init__(self, signals, handler):
-        self.handler = handler
-        self.old_handlers = {}
-        try:
-            self.signals = tuple(signals)
-        except TypeError:
-            self.signals = (signals,)
-
-    def __enter__(self):
-        for signal_type in self.signals:
-            self.old_handlers[signal_type] = signal.getsignal(signal_type)
-            signal.signal(signal_type, self.handler)
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        for signal_type in self.signals:
-            signal.signal(signal_type, self.old_handlers[signal_type])
-
-
-class QuitSignal(Signal):
-    """
-    A context manager that handles system signals by triggering a
-    `threading.Event` instance, giving your program an opportunity to clean up
-    and shut down gracefully. If the signal is repeated a second time, the
-    process quits immediately.
-
-    Parameters
-    ----------
-    signals : int or tuple of int
-        The signal or list of signals to handle.
-    graceful_message : string, optional
-        A message to print to stdout when a signal is received.
-    final_message : string, optional
-        A message to print to stdout before exiting the process when a repeat
-        signal is received.
-
-    Examples
-    --------
-    Quit on SIGINT (ctrl+c) or SIGTERM:
-
-    >>> with QuitSignal((signal.SIGINT, signal.SIGTERM)) as cancel:
-    >>>     for item in some_list:
-    >>>         if cancel.is_set():
-    >>>             break
-    >>>         do_some_work()
-    """
-    def __init__(self, signals, graceful_message=None, final_message=None):
-        self.event = threading.Event()
-        self.graceful_message = graceful_message or (
-            'Attempting to finish existing work before exiting. Press ctrl+c '
-            'to stop immediately.')
-        self.final_message = final_message or (
-            'Stopping immediately and aborting all work!')
-        super().__init__(signals, self.handle_interrupt)
-
-    def handle_interrupt(self, signal_type, frame):
-        if not self.event.is_set():
-            print(self.graceful_message)
-            self.event.set()
-        else:
-            print(self.final_message)
-            os._exit(100)
-
-    def __enter__(self):
-        super().__enter__()
-        return self.event
