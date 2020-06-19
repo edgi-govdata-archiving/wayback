ae3ac23792fdeccf95ce33681f17d0842f6000e5
Rob Brackett <rob@robbrackett.com>
2020-06-19 14:59:02 -0700

    Update & specify sphinx and plugin versions
    We had issues where the official docs generated by ReadTheDocs didn't match docs you'd generate locally because we failed to specify version requirements for Sphinx (and in the absence of a specified version ReadTheDocs will use a very outdated version). We also had some CSS customizations to fix the theme in Sphinx v2+ that did not work in v1. All of these are now fixed by specifying actual version requirements for Sphinx and by updating to the latest of Sphinx and the Sphinx plugins (like the ReadTheDocs theme).
    
    Fixes #35.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,9 +116,7 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/custom.css',
-]
+html_css_files = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
