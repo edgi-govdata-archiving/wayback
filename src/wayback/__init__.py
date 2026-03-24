165558e80c8ef24b5b40c67472c077d764677289
Beckett Frey <beckett.frey@gmail.com>
2026-03-24 14:22:07 -0500

    Enforce single quotes instead

--- src/wayback/__init__.py
+++ src/wayback/__init__.py
@@ -4,9 +4,11 @@ from ._utils import memento_url_data, RateLimit  # noqa
 
 from ._models import (  # noqa
     CdxRecord,
-    Memento)
+    Memento,
+)
 
 from ._client import (  # noqa
     Mode,
     WaybackClient,
-    WaybackSession)
+    WaybackSession,
+)
