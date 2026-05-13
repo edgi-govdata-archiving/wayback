e49909a058fdc7e74ef24ba4143b7dad1ffc5eb4
Rob Brackett <rob@robbrackett.com>
2026-05-12 18:33:54 -0700

    Good linting problems

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import wayback
+
 
 # -- General configuration ------------------------------------------------
 
@@ -72,8 +74,6 @@ author = 'Environmental Data & Governance Initiative'
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-#
-import wayback
 
 # The short X.Y version.
 version = wayback.__version__
