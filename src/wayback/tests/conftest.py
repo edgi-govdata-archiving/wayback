b9b607c0ee66b0f21e17ddd72a4112df647456da
Rob Brackett <rob@robbrackett.com>
2026-06-18 17:49:59 -0700

    Update tests for deprecation

--- src/wayback/tests/conftest.py
+++ src/wayback/tests/conftest.py
@@ -5,11 +5,13 @@ from wayback._utils import RateLimit
 
 @pytest.fixture
 def test_client():
-    session = WaybackSession(
-        search_calls_per_second=RateLimit(0),
-        memento_calls_per_second=RateLimit(0),
-        timemap_calls_per_second=RateLimit(0),
-    )
+    session = None
+    with pytest.warns(DeprecationWarning, match='timemap_calls_per_second'):
+        session = WaybackSession(
+            search_calls_per_second=RateLimit(0),
+            memento_calls_per_second=RateLimit(0),
+            timemap_calls_per_second=RateLimit(0),
+        )
     client = WaybackClient(session)
     yield client
     client.close()
